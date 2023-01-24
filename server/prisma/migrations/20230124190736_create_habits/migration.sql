1cc643e704dccc4c12ae345ae99e79fc5d46eafa
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-24 17:28:52 -0300

    web e server

--- server/prisma/migrations/20230124190736_create_habits/migration.sql
+++ server/prisma/migrations/20230124190736_create_habits/migration.sql
@@ -2,5 +2,5 @@
 CREATE TABLE "habits" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL
+    "created_at" TEXT NOT NULL
 );
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { appRoutes } from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify()
+const prisma = new PrismaClient();
 
-app.register(cors)
-app.register(appRoutes)
+app.register(cors);
 
+app.get('/', () => {
+    return "Hello World"
+})
 
 app.listen({
-    port:3333,
+    port: 3333,
 }).then(() => {
-    console.log(` Server is running`);
+    console.log("Server listening on port 3333")
 })
