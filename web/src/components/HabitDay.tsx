24e9b3cdcd251e070ec51e86c38a10a4fcfd336d
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-25 20:25:38 -0300

    habitos diarios - web

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+interface HabitDayProps {
+    completed: number
+}
+
+export function HabitDay() {
+    return(
+        <div className='bg-zinc-900 w-10 h-10 border-zinc-800 border-2 rounded-lg'>
+            
+        </div>
+    )
+}
