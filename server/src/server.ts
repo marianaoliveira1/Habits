e7bd1eef6472ec1b583c7ba364f9c4464917a663
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-25 16:05:36 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
-import Fastify from "fastify";
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient();
 
-app.register(cors);
-
-app.get('/', () => {
-    return "Hello World"
-})
+app.register(cors)
+app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+  port: 3333,
 }).then(() => {
-    console.log("Server listening on port 3333")
+  console.log('HTTP Server running!')
 })
