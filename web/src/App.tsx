c6d61b458de8c36d0dd8635e78866f80624d267c
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-25 19:26:34 -0300

    header da web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
-import { Habit } from "./components/Habit"
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <div>
-      <Habit completed={3} />
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header />
+        </div>
+      </div>
     </div>
   )
 }
