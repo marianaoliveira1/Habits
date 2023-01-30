cb853b6c0d50d56a22ffa09c2016271a864add62
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-01-30 13:19:53 -0300

    popver dos habitos do dia

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,27 @@
+import * as Popover from "@radix-ui/react-popover"
+import { ProgressBar } from "./ProgressBar"
+
 interface HabitDayProps {
     completed: number
 }
 
 export function HabitDay() {
     return(
-        <div className='bg-zinc-900 w-10 h-10 border-zinc-800 border-2 rounded-lg'>
-            
-        </div>
+        <Popover.Root>
+            <Popover.Trigger className='bg-zinc-900 w-10 h-10 border-zinc-800 border-2 rounded-lg'/> 
+
+            <Popover.Portal>
+                <Popover.Content className="min-w[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+                    <span className="font-semibold text-zinc-400">segunda feira</span>
+                    <span className="mt-1 font-extrabold leading-tight text-3xl">30/01</span>
+
+                    <ProgressBar progress={50} />
+
+                    <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
+
+                </Popover.Content>
+            </Popover.Portal>
+
+        </Popover.Root>
     )
 }
